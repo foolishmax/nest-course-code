11e8571b13c24fa98d87b668a10d7d1ff50dba69
foolishmax <827864735@qq.com>
2023-08-03 20:12:51 +0800

    feat(exception): 统一错误接口问题

--- nest-meeting-room/src/main.ts
+++ nest-meeting-room/src/main.ts
@@ -2,6 +2,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { CustomExceptionFilter } from './custom-exception.filter';
 import { FormatResponseInterceptor } from './format-response.interceptor';
 import { InvokeRecordInterceptor } from './invoke-record.interceptor';
 
@@ -11,6 +12,7 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new FormatResponseInterceptor());
   app.useGlobalInterceptors(new InvokeRecordInterceptor());
+  app.useGlobalFilters(new CustomExceptionFilter());
 
   const configService = app.get(ConfigService);
   await app.listen(configService.get('nest_server_port'));
